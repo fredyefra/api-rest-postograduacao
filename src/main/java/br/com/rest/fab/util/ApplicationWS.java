44ba50db6544b877efbc60244639d96f51005839
fredyefra <fredweb3@gmail.com>
2020-09-03 21:38:56 -0300

    commit ApplicationWS.java

--- src/main/java/br/com/rest/fab/util/ApplicationWS.java
+++ src/main/java/br/com/rest/fab/util/ApplicationWS.java
@@ -11,12 +11,6 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/ws")
 public class ApplicationWS extends Application {
 
-	/*@ApplicationPath: This annotation identifies the application 
-	path that serves as the base URI for all resources. This should 
-	be used on the configuration class, i.e. the subclass of javax.ws.rs.core.Application. 
-	Let's use @ApplicationPath for our example resource, http://example.com/api/orders:*/
-
-	
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> set = new HashSet<>();
